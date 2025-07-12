28174fd0d5d67c3a32a3ca2131cd1974fa46efcb
Ankitrana123456 <ankitrana73jio@gmail.com>
2025-07-12 12:53:09 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-# Don't Remove Credit Tg - WOLVES 🐺
-# Ask Doubt on telegram WOLVES 🐺
+# Don't Remove Credit Tg - ankit 🐺
+# Ask Doubt on telegram ankit🐺
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24731191"))
-API_HASH = environ.get("API_HASH", "ca156f58ad03bb5e350c855bb82e9991")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 AUTH_USERS = "5435086550"
